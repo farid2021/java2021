2549532fee12550cb2983bc4251e17d26e2463c4
farid2021 <faridtchak@gmail.com>
2020-10-03 10:56:24 +0300

    pratice1

---
+++ src/pratice_1/Dog.java
@@ -0,0 +1,37 @@
+package pratice_1;
+
+public class Dog {
+    public class Dog {
+        private int age;
+        private String name;
+
+        public Dog(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+        public int getAge(){
+            return age;
+        }
+        public String getName(){
+            return name;
+        }
+        public void setAge(int age){
+            this.age = age;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public int ConvertInHumanAge(){
+            return age*7;
+        }
+
+        @Override
+        public String toString() {
+            return "pratice_1.Dog{" +
+                    "age=" + age +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+}
